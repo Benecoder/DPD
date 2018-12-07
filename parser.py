41a096db6308f3612584fb50acd7cdd8bd0938be
Benecoder <benedikt.heyl@googlemail.com>
2018-12-07 19:49:26 +0100

    General Improvments

--- parser.py
+++ parser.py
@@ -39,26 +39,51 @@ def find_aoi(content):
     print('finding the areas of interest...')
     
     #finds the measurments
-    trigger = content.logger.values[1:]-content.logger.values[:-1]
-    trigger /= trigger.max()
-    start_marks = (trigger == 1).nonzero()[0]+1
-    end_marks = (trigger == -1).nonzero()[0]
+    trigger = (content.logger.values[1:]-content.logger.values[:-1])+7.0
+    start_marks = (trigger > 0.1).nonzero()[0]+1
+    end_marks = (trigger < 0.1).nonzero()[0]
+    
 
 
     #selects the best measurments
-    measurment_length = 36
+    tolerance = 5 #measurments
     area_of_intrest  = np.zeros(content.shape[0])
+    quality = np.zeros(content.shape[0])
+    cover_column = pd.Series(index =content.index)
+
     for measurment in range(len(start_marks)):
-        best_score = 0
-        for i in range(start_marks[measurment],end_marks[measurment]-measurment_length):
-            p,cov = np.polyfit(np.arange(measurment_length),content.CO2.values[i:i+measurment_length],1,cov = True)
-            if cov[1][0]**2 > best_score:
+        best_score = 1
+        
+        measurment_length = end_marks[measurment]-start_marks[measurment]
+        
+        #if the measurment is shorter than 110 seconds
+        if measurment_length < 110/5:
+            cover = 't' #transparent cover
+            aoi_length = 90/5
+        else:
+            cover = 'd' #dark cover
+            if measurment_length < 200/5:
+                aoi_length = 180/5
+            else:
+                aoi_length = 300/5
+                
+        
+        for i in range(start_marks[measurment]-tolerance,end_marks[measurment]+tolerance-aoi_length):
+            p,cov = np.polyfit(np.arange(aoi_length),content.CO2.values[i:i+aoi_length],1,cov = True)
+            if cov[1][0]**2 < best_score:
                 best_score = cov[1][0]**2
                 best_index = i
-        if best_score != 0:
-            area_of_intrest[best_index:best_index+measurment_length] = 1
-            
-    return area_of_intrest
+                best_slope = p[0]
+
+        if best_score != 1:
+            area_of_intrest[best_index:best_index+aoi_length+1] = 1
+            cover_column[best_index:best_index+aoi_length+1] = cover
+            if cover == 'd' and best_slope < 0:
+                quality[best_index:best_index+aoi_length+1] = 2
+            else:
+                quality[best_index:best_index+aoi_length+1] = 1
+
+    return area_of_intrest,cover_column,quality
 
 #visualizing
 def vis(content,area_of_intrest):
@@ -82,23 +107,24 @@ def vis(content,area_of_intrest):
     plt.xlabel('Measurment_index')
 
     plt.show()
-    
 
 
 #building the new file
 #final header 
-def write_file(content,area_of_intrest):
+def write_file(content,area_of_intrest,cover_column,quality):
     
     print('Writing the final file...')
 
     result = pd.DataFrame(index = content.index)
     result['date'] = [x.date() for x in content.timestamp]
     result['time'] = [x.time() for x in content.timestamp]
-    result['CO2'] = content.CO2
     result['PAR'] = content.PAR
-    result['temp'] = content.temp
-    result['logger'] = content.logger
+    result['CO2'] = content.CO2
+    result['temperature'] = content.temp
+    result['chamber'] = cover_column
+    result['Logger'] = content.logger
     result['AoI'] = area_of_intrest
+    result['quality'] = quality
     result['H2O'] = content.H2O
 
     writer = pd.ExcelWriter('result.xlsx')
@@ -107,7 +133,8 @@ def write_file(content,area_of_intrest):
 
 if __name__  == '__main__':
     content = read_file(sys.argv[1])
-    area_of_intrest = find_aoi(content)
-    #vis(content,area_of_intrest)
-    write_file(content,area_of_intrest)
+    area_of_intrest,cover_column,quality = find_aoi(content)
+ #   vis(content,area_of_intrest)
+ 
+    #write_file(content,area_of_intrest,cover_column,quality)
     

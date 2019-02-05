bca86f10997237afbb2708e8ce487a747d858b3a
Benecoder <Benedikt.Heyl@googlemail.com>
2019-02-06 00:44:01 +0100

    update dependencies

--- test.py
+++ test.py
@@ -1,9 +1,10 @@
 #!/usr/bin/env python3
 
-import numpy 
-import pandas
+import pandas as pd
+import numpy as np
 import sys
 import os
+import argparse
 
 print('All nescessary libraries are present.')
 
